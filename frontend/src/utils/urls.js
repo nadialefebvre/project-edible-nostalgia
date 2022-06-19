87d875f3bedd769d0e0715c6aa85d17e4c00022b
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-06-19 16:59:17 +0200

    changed url

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080"
-// const BASE_URL = "https://final-project-nadia.herokuapp.com"
+// const BASE_URL = "http://localhost:8080"
+const BASE_URL = "https://nadia-final-project.herokuapp.com"
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
