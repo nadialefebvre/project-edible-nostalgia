9271fc0b5ae5631bc0ff087feb3d96d96b0d18f1
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-06-19 14:37:50 +0200

    added table with recipes

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -5,12 +5,20 @@ import { useNavigate } from "react-router-dom"
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import Divider from '@mui/material/Divider'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
+import Link from '@mui/material/Link'
 
 import Hero from '../components/Hero'
 import EditDelete from "../components/EditDelete"
 import { API_URL } from "../utils/urls"
 import user from "../reducers/user"
-
+import loading from "../reducers/loading"
 
 const Profile = () => {
   const navigate = useNavigate()
@@ -27,12 +35,6 @@ const Profile = () => {
     image: 'https://source.unsplash.com/random',
     imageText: 'main image description',
   }
-
-  // useEffect(() => {
-  //   if (!accessToken) {
-  //     navigate("/")
-  //   }
-  // }, [accessToken])
 
 
   const handleDeleteProfile = () => {
@@ -59,6 +61,37 @@ const Profile = () => {
     setOpen(true)
   }
 
+  const isLoading = useSelector((store) => store.loading.isLoading)
+
+  const [userRecipes, setUserRecipes] = useState([])
+
+
+  useEffect(() => {
+    dispatch(loading.actions.setLoading(true))
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": accessToken,
+      },
+    }
+
+    fetch(API_URL(`recipes/user/${userId}`), options)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          setUserRecipes(data.response)
+        } else {
+          alert(data.response.message)
+        }
+        dispatch(loading.actions.setLoading(false))
+      })
+  }, [userRecipes])
+
+
+
+
+
   return (
     <>
       <Hero hero={hero} />
@@ -83,6 +116,41 @@ const Profile = () => {
           {accessToken ? firstName : "Create an account to have access to XYZ here"}
         </Typography>
       </Grid>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Recipe</TableCell>
+              <TableCell align="right">Servings</TableCell>
+              <TableCell align="right">Category</TableCell>
+              <TableCell align="right">Baking time</TableCell>
+
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {userRecipes.map((recipe) => (
+              <TableRow
+                key={recipe.title}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  <Link
+                    href={`/recipes/${recipe._id}`}
+                    onClick={() => navigate(`/recipes/${recipe._id}`)}
+                    color="inherit"
+                    underline="hover"
+                  >
+                    {recipe.title}
+                  </Link>
+                </TableCell>
+                <TableCell align="right">{recipe.servings}</TableCell>
+                <TableCell align="right">{recipe.category}</TableCell>
+                <TableCell align="right">{recipe.bakingTime}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   )
 }
