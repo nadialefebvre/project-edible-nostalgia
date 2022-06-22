88c77eca5970b4fffc6b833ff94717d798953f18
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-06-22 20:27:55 +0200

    clean

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,19 +1,11 @@
-// const BASE_URL = "http://localhost:8080"
 const BASE_URL = "https://nadia-final-project.herokuapp.com"
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
 
-
 export const imageToUse = (category) => {
-  // if (category === "Whatever") {
-  //   return "https://res.cloudinary.com/devnadia/image/upload/v1655652905/Whatever_aep1gl.jpg"
-  // } else {
-  //   return "https://images.unsplash.com/photo-1577048982771-1960014bde8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80"
-  // }
-
   switch (category) {
     case "Homesick":
-      return "https://res.cloudinary.com/devnadia/image/upload/v1655655685/maple_f8n3r1.jpg"
+      return "https://res.cloudinary.com/devnadia/image/upload/v1655920772/homesick_visq6w.jpg"
     case "Childhood":
       return "https://res.cloudinary.com/devnadia/image/upload/v1655919414/childhood_xgzqap.jpg"
     case "Birthday":
@@ -24,11 +16,13 @@ export const imageToUse = (category) => {
       return "https://res.cloudinary.com/devnadia/image/upload/v1655918880/grandma_vvdow5.jpg"
     case "Friendship":
       return "https://res.cloudinary.com/devnadia/image/upload/v1655919414/friendship_rm9yqu.jpg"
-        case "Love":
+    case "Love":
       return "https://res.cloudinary.com/devnadia/image/upload/v1655919546/love_u8n8fw.jpg"
-
-
+    case "Travel":
+      return "https://res.cloudinary.com/devnadia/image/upload/v1655922081/travel_y3lbnm.jpg"
+    case "Other":
+      return "https://res.cloudinary.com/devnadia/image/upload/v1655922081/viewmaster_p4vsth.jpg"
     default:
-      return ""
+      return "https://res.cloudinary.com/devnadia/image/upload/v1655658415/nostalgia_qk8prs.jpg"
   }
 }
