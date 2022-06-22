d4cbc742fbb8063b18f00374c14a3bb950fad624
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-06-22 20:27:30 +0200

    clean

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,11 @@
 import React from "react"
-import Grid from '@mui/material/Grid'
-import Typography from '@mui/material/Typography'
-import Divider from '@mui/material/Divider'
+import Grid from "@mui/material/Grid"
+import Typography from "@mui/material/Typography"
+import Divider from "@mui/material/Divider"
 
-import Hero from '../components/Hero'
+import Hero from "../components/Hero"
 
-const Home = ({hero}) => {
+const Home = ({ hero }) => {
 
   return (
     <>
@@ -15,10 +15,14 @@ const Home = ({hero}) => {
           Edible nostalgia
         </Typography>
         <Divider />
-        <Typography variant="p">
-        Food holds a special place in our brain, as it is deeply connected to 
-        all our reminiscences. Collect your precious recipes here, and share 
-        them with the world if you wish.
+        <Typography variant="p" paragraph>
+          Food holds a special place in our brain, as it is deeply connected to
+          all our reminiscences. Collect your precious recipes here, and share
+          them with the world if you wish.
+        </Typography>
+        <Typography variant="p" >
+          This website is the final project for Technigo's frontend development 
+          bootcamp.
         </Typography>
       </Grid>
     </>
