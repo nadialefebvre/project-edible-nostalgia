286bcb2e9c31a26ef7987bc351bd4e73875addc2
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-06-22 20:11:06 +0200

    clean

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,29 +2,23 @@ import React from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Provider } from "react-redux"
 import { combineReducers, configureStore } from "@reduxjs/toolkit"
-import CssBaseline from '@mui/material/CssBaseline'
-import Container from '@mui/material/Container'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
+import CssBaseline from "@mui/material/CssBaseline"
+import Container from "@mui/material/Container"
+import { ThemeProvider } from "@mui/material/styles"
 
-
-import Header from './components/Header'
-import Footer from './components/Footer'
-
-
-import loading from "reducers/loading"
-import user from "./reducers/user"
-import Error404 from "pages/Error404"
-import Profile from "./pages/Profile"
 import Home from "./pages/Home"
+import Recipes from "./pages/Recipes"
 import SingleRecipe from "./pages/SingleRecipe"
 import RecipeForm from "./pages/RecipeForm"
+import Profile from "./pages/Profile"
 import UserForm from "./pages/UserForm"
-
-// const theme = createTheme()
-
-import { theme } from "./utils/theme"
+import Error404 from "pages/Error404"
 import AccessDenied from "./pages/AccessDenied"
-import Recipes from "./pages/Recipes"
+import loading from "reducers/loading"
+import user from "./reducers/user"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import { theme } from "./utils/theme"
 
 const reducer = combineReducers({
   loading: loading.reducer,
@@ -44,12 +38,6 @@ store.subscribe(() => {
   localStorage.setItem("state", JSON.stringify(store.getState()))
 })
 
-const hero = {
-  title: "Edible nostalgia",
-  image: 'https://res.cloudinary.com/devnadia/image/upload/v1655658415/nostalgia_qk8prs.jpg'
-}
-
-
 const App = () => {
   return (
     <Provider store={store}>
@@ -62,15 +50,15 @@ const App = () => {
               <Routes>
                 <Route path="/recipes/:recipeId" element={<SingleRecipe />} />
                 <Route path="/recipes/add" element={<RecipeForm />} />
-                <Route path="/recipes" element={<Recipes hero={hero} />} />
+                <Route path="/recipes" element={<Recipes />} />
                 <Route path="/recipes/:recipeId/edit" element={<RecipeForm />} />
                 <Route path="/login" element={<UserForm />} />
-                <Route path="/profile" element={<Profile hero={hero} />} />
+                <Route path="/profile" element={<Profile />} />
                 <Route path="/profile/edit" element={<UserForm />} />
-                <Route path="/" element={<Home hero={hero} />} />
-                <Route path="*" element={<Error404 hero={hero} />} />
-                <Route path="/error404" element={<Error404 hero={hero} />} />
-                <Route path="/accessdenied" element={<AccessDenied hero={hero} />} />
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={<Error404 />} />
+                <Route path="/error404" element={<Error404 />} />
+                <Route path="/accessdenied" element={<AccessDenied />} />
               </Routes>
             </main>
             <Footer />
